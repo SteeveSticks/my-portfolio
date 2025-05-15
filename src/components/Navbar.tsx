6fa9b7df53210d3e8179d58a45bdd3de7ec35c55
Stephen Adebanjo <stephenadebanjo86@gmail.com>
2025-05-15 23:43:42 +0100

    navbar 0.1

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Button } from "./ui/button";
 import Link from "next/link";
+import { FiGithub } from "react-icons/fi";
+import { IoDocumentTextOutline } from "react-icons/io5";
+import { HoverCard, HoverCardTrigger, HoverCardContent } from "./ui/hover-card";
 
 const Navbar = () => {
   return (
-    <header className="border ">
-      <nav className="max-w-[526px] border mx-auto py-2 bg-white flex justify-between items-center">
+    <header className="py-4">
+      <nav className="max-w-[586px] border mx-auto bg-black/85 flex justify-between items-center py-2 px-4 rounded-full">
         <div>
           <img src="img/brandLogo.png" alt="Brand Logo" />
         </div>
@@ -30,10 +33,25 @@ const Navbar = () => {
           </ul>
         </div>
 
-        <div className="border-l">
-          <Button className="ml-2">
-            <Link href="/">Contact</Link>
-          </Button>
+        <div className="flex-between gap-2">
+          <HoverCard>
+            <HoverCardTrigger>
+              <FiGithub className="size-6 text-white hover:text-white/90" />
+            </HoverCardTrigger>
+            <HoverCardContent>GitHub</HoverCardContent>
+          </HoverCard>
+          <HoverCard>
+            <HoverCardTrigger>
+              <IoDocumentTextOutline className="size-6 text-white hover:text-white/90" />
+            </HoverCardTrigger>
+            <HoverCardContent>Resume</HoverCardContent>
+          </HoverCard>
+
+          <div className="border-l">
+            <Button className="ml-2">
+              <Link href="/">Contact</Link>
+            </Button>
+          </div>
         </div>
       </nav>
     </header>
