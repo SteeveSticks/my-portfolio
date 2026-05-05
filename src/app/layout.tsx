2ce1fbdaae8c708ada618af2b3159cc24b861371
Stephen Adebanjo <stephenadebanjo86@gmail.com>
2026-05-05 02:53:52 +0100

    Implement vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Navbar from "@/components/Navbar";
 import { Toaster } from "@/components/ui/sonner";
 import Footer from "@/components/Footer";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -143,6 +144,7 @@ export default function RootLayout({
           <Navbar />
           {children}
           <Footer />
+          <Analytics/>
         </main>
         <Toaster />
       </body>
